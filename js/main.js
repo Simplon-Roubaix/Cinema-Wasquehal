2d709cfde9c3e485489fec41624cc77fc5638560
Manwell <lec.manoel@gmail.com>
2016-01-08 15:19:34 +0100

    recup last

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+	$('#bootstrap-example').quickselect({
+		activeButtonClass: 'btn-primary active',
+		breakOutAll: true,
+		buttonClass: 'btn btn-default',
+		selectDefaultText: 'Other',
+		wrapperClass: 'btn-group'
+	});
