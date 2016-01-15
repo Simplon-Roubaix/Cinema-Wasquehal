411ccb7eb6b3734f09951e9b4cc8a44738ef88f0
thomascav <tcavalar@numericable.fr>
2016-01-15 13:44:16 +0100

    fixed aside problem with resized page

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
-//Menu accordéon
-// $(document).ready(function(){
-// 	$("#accordian h3").click(function(){
-//
-// 		//slide up all the link lists
-// 		$("#accordian ul ul").slideUp();
-// 		//slide down the link list below the h3 clicked - only if its closed
-// 		if(!$(this).next().is(":visible"))
-// 		{
-// 			$(this).next().slideDown();
-// 		}
-// 	})
-// })
-//Fin menu accordéon
-
 //New accordion
 $('.toggle').click(function(e) {
   	e.preventDefault();
