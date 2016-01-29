43ca036372ef34af7330742a00008d304b83c65c
didierClin <didier.c62@numericable.fr>
2016-01-29 12:23:09 +0100

    Boucle sur le fichier json

--- js/gereFilm.js
+++ js/gereFilm.js
@@ -4,44 +4,30 @@ function showUsersInfo(){
 
     console.log(film);
       $.each( film, function( key, donnee ) {
-
+          var ident = key;
+          console.log(key);
          // Iteration sur les data d'un film
-            // $("<div>" + key + "</div>").appendTo( "#vue" );
             $.each( donnee, function( pkey, pdonnee ) {
               // $("<div>" + pkey + " = " + value + "</div>").appendTo( "#vue" );
               // Mon code:
+              console.log(key)
               switch (pkey) {
                 case "poster_path" :
-                        $('#aff_film1').attr('src',pdonnee);
+                        $('#aff_'+key).attr('src',pdonnee);
                         break;
                 case "title":
-                        $('#title1').text(pdonnee);
-                        $('#aff_film1').attr({alt: 'affiche du film '+pdonnee, title: 'affiche du film '+pdonnee, height: '600px;'});
+                        $('#title_'+key).text(pdonnee);
+                        $('#aff_'+key).attr({alt: 'affiche du film '+pdonnee, title: 'affiche du film '+pdonnee, height: '600px;'});
                         break;
                 case "overview":
-                        $('#overview1').text(pdonnee);
+                        $('#overview_'+key).text(pdonnee);
                         break;
                 case "video":
-                        $('#video1').attr('src',pdonnee);
+                        $('#video_'+key).attr('src',pdonnee);
                         break;
                 default:
                         break;
               }
-                    // if (pkey=="poster_path"){
-                    //               $('#aff_film1').attr('src',pdonnee);
-                    //             //  $('#aff_film1').attr({src: pdonnee, heigth: '800px'});
-                    //             // $('#film1').text("CROCODILE ATTACK!");
-                    // }
-                    // if (pkey=="title"){
-                    //              $('#title1').text(pdonnee);
-                    //              $('#aff_film1').attr({alt: 'affiche du film '+pdonnee, title: 'affiche du film '+pdonnee});
-                    // }
-                    // if (pkey=="overview"){
-                    //              $('#overview1').text(pdonnee);
-                    // }
-                    // if (pkey=="video"){
-                    //              $('#video1').attr('src',pdonnee);
-                    // }
              });
             // $("<div>___</div>").appendTo( "#vue" );
       });
